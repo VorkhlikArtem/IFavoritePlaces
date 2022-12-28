3c62f5b042ce3e2727b6f854a578afe7f084fd38
artem <artemvorkhlik@mail.ru>
2022-12-28 17:53:19 +0300

    custom Rating control

--- IFood/ViewController.swift
+++ IFood/ViewController.swift
@@ -11,9 +11,34 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        let ratingControl = RatingControl()
+        ratingControl.backgroundColor = .cyan
+        ratingControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(ratingControl)
+        ratingControl.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        ratingControl.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
     }
 
 
 }
 
+//MARK: - SwiftUI
+import SwiftUI
+struct ViewControllerProvider: PreviewProvider {
+    static var previews: some View {
+        ContainerView().ignoresSafeArea(.all)
+    }
+    struct ContainerView: UIViewControllerRepresentable {
+        
+        let viewController = ViewController()
+        
+        func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        }
+        
+        func makeUIViewController(context: Context) -> some UIViewController {
+            return viewController
+        }
+    }
+}
